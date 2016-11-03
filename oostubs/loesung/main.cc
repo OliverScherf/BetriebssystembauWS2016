8ed8d5715b0571b41e3d8ae3e747b8d37a82555b
shiverspell <vanessaboehrk@googlemail.com>
2016-11-03 16:44:15 +0100

    no idea!

--- oostubs/loesung/main.cc
+++ oostubs/loesung/main.cc
@@ -14,33 +14,32 @@ int main()
     kout << "Unsigned Short 255 (dec): " << o_sm::dec << (unsigned short)255 <<o_sm::endl;
     kout << "Unsigned Short 255 (hex): " << o_sm::hex << (unsigned short)255 <<o_sm::endl;
     kout << o_sm::endl;
-    kout << "Long -13 (bin): " << o_sm::bin << (long)-13 <<o_sm::endl;
-    kout << "Short -13 (bin): " << o_sm::bin << (short)-13 <<o_sm::endl;
-    //kout.flush();
-    kout << "Long -1337 (oct): " << o_sm::oct << (long)-1337 <<o_sm::endl;
-    //kout.flush();
-    kout << "Long -1337 (dec): " << o_sm::dec << (long)-1337 <<o_sm::endl;
-    //kout.flush();
-    kout << "Long -1337 (hex): " << o_sm::hex << (long)-1336 <<o_sm::endl;
-    //kout.flush();
+
+    kout << "Int -1337 (bin): " << o_sm::bin << -1337 <<o_sm::endl;
+    kout << "Int -1337 (oct): " << o_sm::oct << -1337 <<o_sm::endl;
+    kout << "Int -1337 (dec): " << o_sm::dec << -1337 <<o_sm::endl;
+    kout << "Int -1337 (hex): " << o_sm::hex << -1336 <<o_sm::endl;
     kout << o_sm::endl;
+
     kout << "long 1234567890L (bin): " << o_sm::bin << 1234567890L <<o_sm::endl;
     kout << "long 1234567890L (oct): " << o_sm::oct << 1234567890L <<o_sm::endl;
     kout << "long 1234567890L (dec): " << o_sm::dec << 1234567890L <<o_sm::endl;
     kout << "long 1234567890L (hex): " << o_sm::hex << 1234567890L <<o_sm::endl;
     kout << "char* 0xb8000: " << o_sm::dec << (int*)0xb8000 << o_sm::endl;
-    kout.flush();
 
-    kout.clear();
+    //kout.clear();
+    //Demo for screen clearing and scrolling
+    for(unsigned int i=0; i<25; i++)
+        kout << "Scrolling test" << endl;
 
-    Keyboard_Controller keyctl;
-    Key tmp;
-    for (;;) {
-    	tmp = keyctl.key_hit();
-    	if (tmp.valid()) {
-			kout << tmp.ascii();
-			kout.flush();
-    	}
-    }
+    //Keyboard_Controller keyctl;
+    //Key tmp;
+    //for (;;) {
+    //	tmp = keyctl.key_hit();
+    //	if (tmp.valid()) {
+	//		kout << tmp.ascii();
+	//		kout.flush();
+    //	}
+    //}
     return 0;
  }
