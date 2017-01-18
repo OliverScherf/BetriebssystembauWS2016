bb727e75ca7f5cfcf5552a8f80c084f7e4b54150
shiverspell <vanessaboehrk@googlemail.com>
2017-01-18 16:47:58 +0100

    Applications außerhalb der Main

--- oostubs/loesung/dep/main.d
+++ oostubs/loesung/dep/main.d
@@ -1,9 +1,9 @@
 build/main.o: main.cc user/appl.h device/cgastr.h object/o_stream.h \
  object/strbuf.h machine/cgascr.h machine/io_port.h machine/io_port.h \
  guard/secure.h guard/guard.h object/queue.h object/chain.h \
- guard/locker.h machine/cpu.h syscall/thread.h thread/entrant.h \
- thread/coroutine.h machine/toc.h machine/cpu.h device/cgastr.h \
- machine/plugbox.h guard/gate.h device/panic.h device/panic.h \
+ guard/locker.h machine/cpu.h guard/gate.h syscall/thread.h \
+ thread/entrant.h thread/coroutine.h machine/toc.h machine/cpu.h \
+ device/cgastr.h machine/plugbox.h device/panic.h device/panic.h \
  device/keyboard.h machine/keyctrl.h machine/key.h machine/plugbox.h \
  machine/pic.h machine/pic.h syscall/guarded_scheduler.h \
  thread/scheduler.h thread/dispatch.h device/watch.h machine/pit.h \
